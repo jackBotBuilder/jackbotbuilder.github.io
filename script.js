46dd7170a681c8cc5fae0b72022d28626a7f459f
jackBotBuilder <36828116+jackBotBuilder@users.noreply.github.com>
2022-09-16 22:04:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var cssToggle = document.getElementById("cssShift");
 
 function buttonToggle(){
 document.innerHTML = "Flurkin Kitty";
+  cssToggle.innerHTML = "Flurkin Kitty";
 
 };
 cssToggle.innerHTML = "Flurkin Kitty!!!";
