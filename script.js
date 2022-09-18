148b4ba707dc1cc3daa833c9f1abfe4893a04f1b
jackBotBuilder <36828116+jackBotBuilder@users.noreply.github.com>
2022-09-17 22:51:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ var cssToggle = document.getElementById("cssShift");
 var stylesheet = document.getElementById("stylesheet");
 
 function buttonToggle(){
-    cssToggle.innerHTML = "Update 1:";
-    stylesheet.href = "styles/alternate.css";
+    if (cssToggle.innerHTML == "Normal Css"){
+    cssToggle.innerHTML = "Special Surprise Toggle";
+    stylesheet.href = "styles/style.css";
+    }
+    else {
+    cssToggle.innerHTML = "Normal Css";
+    stylesheet.href = "styles/alternate.css";}
+    
 };
-cssToggle.innerHTML = "Special Suprise Toggle";
+
 
 cssToggle.addEventListener("click", buttonToggle);
